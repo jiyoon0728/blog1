ec7963c162a759fece6cdab20f51942fe4097d42
ji yoon <wldbf11111@naver.com>
2023-02-12 23:28:25 +0900

    컨트롤러,jsp 완료

--- src/main/java/shop/mtcoding/abc/model/BoardRepository.java
+++ src/main/java/shop/mtcoding/abc/model/BoardRepository.java
@@ -1,5 +1,25 @@
 package shop.mtcoding.abc.model;
 
-public class BoardRepository {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface BoardRepository {
+
+    public List<Board> findAll();
+
+    public Board findById(int id);
+
+    public List<Board> findByUserId(int userId);
+
+    public int insert(@Param("title") String title, @Param("content") String content,
+            @Param("thumbnail") String thumbnail,
+            @Param("userId") int userId);
+
+    public int updateById(@Param("id") int id, @Param("title") String title, @Param("thumbnail") String thumbnail,
+            @Param("content") String content);
+
+    public int deleteById(int id);
 }
--- src/main/java/shop/mtcoding/abc/model/UserRepository.java
+++ src/main/java/shop/mtcoding/abc/model/UserRepository.java
@@ -1,5 +1,25 @@
 package shop.mtcoding.abc.model;
 
-public class UserRepository {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserRepository {
+
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String email);
+
+    public int updateById(@Param("id") int id, @Param("password") String password, @Param("email") String email);
+
+    public int deleteById(int id);
+
+    public List<User> findAll();
+
+    public User findById(int id);
+
+    public User findByUsername(@Param("username") String username);
+
+    public User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
