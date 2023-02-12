18f9f12ad4fe1ab8d85a8d76e0421a2695d5a9bb
ji yoon <wldbf11111@naver.com>
2023-02-12 22:32:28 +0900

    세팅완료

---
+++ src/main/java/shop/mtcoding/abc/Home.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.abc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/shop/mtcoding/abc/AbcApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.abc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
