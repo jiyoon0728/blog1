668f9ebdaa9fc79b914b120ff7150015f08e144a
ji yoon <wldbf11111@naver.com>
2023-02-13 01:29:45 +0900

    회원가입 테스트 완료

--- src/main/java/shop/mtcoding/abc/service/UserService.java
+++ src/main/java/shop/mtcoding/abc/service/UserService.java
@@ -1,7 +1,10 @@
 package shop.mtcoding.abc.service;
 
+import org.springframework.stereotype.Service;
+
 import shop.mtcoding.abc.dto.user.userReq.JoinReqDto;
 
+@Service
 public class UserService {
     public int 회원가입(JoinReqDto joinReqDto) {
         return -1;
